00cba77f0d9c4bd2bddcdc355ab5182ddffd063f
preke <geek00021@gmail.com>
2018-05-02 20:21:26 +0800

    update

--- code/cnn/train.py
+++ code/cnn/train.py
@@ -81,6 +81,7 @@ def eval(data_iter, model, args):
         for i in range(length):
             a = logit[i]
             b = target[i]
+            print a, ' ', b
             if a < 0.5 and b == 0:
                 corrects += 1
             elif a >= 0.5 and b == 1:
