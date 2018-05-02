45f6bdc66c0996357656809ec235ed3e34bb69da
preke <geek00021@gmail.com>
2018-05-02 09:25:17 +0800

    add jacarrd

---
+++ code/simple_jacarrd.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import pandas as pd
+import numpy as np
+
+
+DATA_PATH = '../data/split_result.csv'
+
+def Jacarrd(vec1, vec2):
+    '''
+    vec1 and vec2 are vectors of splited sentences.
+    '''
+    up = float(len(set(vec1) & set(vec2)))
+    down = float(len(set(vec1) | set(vec2)))
+    return up/down
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(open(DATA_PATH, 'rU'), encoding='utf-8')
+    df['wordList1'] = df['wordList1'].apply(lambda x: s.split('#'))
+    df['wordList2'] = df['wordList2'].apply(lambda x: s.split('#'))
+    df['Jacarrd_res'] = df.apply(lambda x: Jacarrd(x['wordList1'], x['wordList2']))
+
+    thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    length = len(df)
+    for threshold in thresholds:
+        tmp_acc = 0
+        for i, r in df.iterrows():
+            if r['is_duplicated'] == 1 and r['Jacarrd_res'] >= threshold:
+                tmp_acc += 1
+            elif r['is_duplicated'] == 0 and r['Jacarrd_res'] < threshold:
+                tmp_acc += 1
+        print('Threshold is %s, Accuracy is %s' %(str(threshold), str(tmp_acc/length)))
