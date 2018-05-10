2b53ebc416536cbcd55f09137295384725c46031
preke <geek00021@gmail.com>
2018-05-10 15:15:38 +0800

    .

--- code/cnn/main.py
+++ code/cnn/main.py
@@ -33,7 +33,7 @@ parser.add_argument('-shuffle', action='store_true', default=False, help='shuffl
 # model
 parser.add_argument('-dropout', type=float, default=0.5, help='the probability for dropout [default: 0.5]')
 parser.add_argument('-max-norm', type=float, default=3.0, help='l2 constraint of parameters [default: 3.0]')
-parser.add_argument('-embed-dim', type=int, default=100, help='number of embedding dimension [default: 300]')
+parser.add_argument('-embed-dim', type=int, default=300, help='number of embedding dimension [default: 300]')
 parser.add_argument('-kernel-num', type=int, default=100, help='number of each kind of kernel')
 parser.add_argument('-kernel-sizes', type=str, default='3', help='comma-separated kernel size to use for convolution')
 parser.add_argument('-static', action='store_true', default=True, help='fix the embedding')
@@ -55,7 +55,7 @@ text_field, label_field, train_data, train_iter,\
 
 
 args.embed_num = len(text_field.vocab)
-args.embed_dim = 100
+args.embed_dim = 300
 args.word_Embedding = True
 
 embedding_dict = Word2Vec.load(w2v_model_path)
