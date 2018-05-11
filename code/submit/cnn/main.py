75aa92b9ac46a02b7c4f17d1f70df8fa23ed977b
preke <geek00021@gmail.com>
2018-05-11 14:48:27 +0800

    .

--- code/submit/cnn/main.py
+++ code/submit/cnn/main.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     oov = 0
     for idx, word in enumerate(text_field.vocab.itos):
         try:
-            vector = np.array(embedding_dict[word], dtype=float).reshape(1, args.embed_dim)
+            vector = np.array(embedding_dict[str(word.encode('utf-8'))], dtype=float).reshape(1, args.embed_dim)
         except:
             print(word)
             oov += 1
