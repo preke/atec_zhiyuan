32aed418b84a732f20b02f1a3ae2b5e4958b5392
preke <geek00021@gmail.com>
2018-05-11 18:33:12 +0800

    .

--- code/submit/data/gen_test.py
+++ code/submit/data/gen_test.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 train_df = pd.read_csv('atec_nlp_sim_train.csv', sep='\t', names=['id', 'q1', 'q2', 'label'])
 test_df = train_df[['id', 'q1', 'q2']]
-test_df.to_csv('test.csv', index=False, header=None)
+test_df.to_csv('test.csv', index=False, header=None, sep='\t')
