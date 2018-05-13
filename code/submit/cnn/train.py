2a646c44fbab03ebb4f0485803eb0435603bd943
preke <geek00021@gmail.com>
2018-05-13 20:54:36 +0800

    .

--- code/submit/cnn/train.py
+++ code/submit/cnn/train.py
@@ -85,7 +85,7 @@ def eval(data_iter, model, args):
         for i in range(length):
             a = logit[i].data.cpu().numpy()
             b = target[i].data.cpu().numpy()
-            print('%s,   %s' %(str(a), str(b)))
+            # print('%s,   %s' %(str(a), str(b)))
             if a < 0.5 and b == 0:
                 corrects += 1
             elif a >= 0.5 and b == 1:
