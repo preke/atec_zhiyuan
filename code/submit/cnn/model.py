e732dbe6275d28310102b0736aa820251750a4cc
preke <geek00021@gmail.com>
2018-05-13 19:55:51 +0800

    .

--- code/submit/cnn/model.py
+++ code/submit/cnn/model.py
@@ -37,8 +37,8 @@ class CNN_Text(nn.Module):
         q1 = [F.tanh(conv(q1)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
         q1 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q1]  # [(N, Co), ...]*len(Ks)
         q1 = [F.tanh(i) for i in q1]
-        q1 = self.fc1(q1)
-        q1 = self.dropout(q1)
+        # q1 = self.fc1(q1)
+        # q1 = self.dropout(q1)
         q1 = [self.fc1(q) for q in q1]
         q1 = [self.dropout1(q) for q in q1]
         q1 = [self.fc2(q) for q in q1]
