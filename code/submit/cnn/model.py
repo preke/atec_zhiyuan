36747deeba606d2c5d54014879f53f82eee1e441
preke <geek00021@gmail.com>
2018-06-28 16:18:57 +0800

    .

--- code/submit/cnn/model.py
+++ code/submit/cnn/model.py
@@ -25,14 +25,14 @@ class CNN_Text(nn.Module):
 
     
     def forward(self, q1):
-        q1 = self.embed(q1) # n * d
-        
-        q1 = q1.unsqueeze(1)  # n * 1 * d
-        
-        q1 = F.tanh(self.conv1(q1)).squeeze(3)  #
+        q1 = self.embed(q1) # 32 * n * d
         print q1.shape
-        # q1 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q1]  # [(N, Co), ...]*len(Ks)
-        
+        q1 = q1.unsqueeze(1)  # 32 * 1 * n * d
+        print q1.shape
+        q1 = F.tanh(self.conv1(q1)).squeeze(3)  # 32 * d * n
+        print q1.shape
+        q1 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q1]  # [(N, Co), ...]*len(Ks)
+        print q1.shape
         # q1 = [F.tanh(conv(q1)).squeeze(3) for conv in self.convs1]  # [(N, Co, W), ...]*len(Ks)
         # q1 = [i.size(2) * F.avg_pool1d(i, i.size(2)).squeeze(2) for i in q1]  # [(N, Co), ...]*len(Ks)
         
