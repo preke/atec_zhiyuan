ad2e5cd87e616e8312b2f476fd0cfb7d5e6eeefb
preke <geek00021@gmail.com>
2018-06-28 23:31:27 +0800

    .

--- code/submit/cnn/model.py
+++ code/submit/cnn/model.py
@@ -33,9 +33,12 @@ class CNN_Text(nn.Module):
         print q1.shape
         q1 = F.tanh(self.conv1(q1))  # batch_size * out_channel * n-2
         print q1.shape
-        q1 = F.avg_pool1d(q1, self.K)
-        # q1 = q1.squeeze(3)
+        q1 = q1.squeeze(3)
         print q1.shape
+        q1 = F.avg_pool1d(q1, self.K)
+        print q1.shape
+
+        
         # q1 = F.avg_pool1d(q1, q1.size(2)).squeeze(2) # batch_size * out_channel
         q1 = F.max_pool1d(q1, q1.size(2)) # .squeeze(2) # batch_size * out_channel
         print q1.shape
