2b984b95a3296a17a97b2a51039fd9e2ecd879d1
preke <geek00021@gmail.com>
2018-07-02 20:13:48 +0800

    .

---
+++ code/submit/abcnn/load_data.py
@@ -0,0 +1,117 @@
+# coding = utf-8
+import os
+import pandas as pd
+import numpy as np
+import re
+import random
+from env.torchtext import data
+from datetime import datetime
+import traceback  
+import env.torchtext.datasets as datasets
+import pickle
+from gensim.models import Word2Vec
+import jieba
+
+jieba.load_userdict('data/special_word.txt')
+w2v_model_path = 'data/w2v_train.save'
+
+def train_word2vec_model(df):
+    '''
+    basic w2v model trained by sentences
+    '''
+    corpus = []
+    for i, r in df.iterrows():
+        try:
+            corpus.append(jieba.lcut(r['ques1']))
+            # print jieba.lcut(r['ques1'])
+            corpus.append(jieba.lcut(r['ques2']))
+        except:
+            print 'Exception: ', r['ques1']
+    word2vec_model = Word2Vec(corpus, size=300, window=3, min_count=1, sg=0, iter=100)
+    return word2vec_model
+
+
+def load_glove_as_dict(filepath):
+    word_vec = {}
+    with open(filepath) as fr:
+        for line in fr:
+            line = line.split()
+            word = line[0]
+            vec = line[1:]
+            word_vec[word] = vec
+    return word_vec
+
+def gen_iter(path, text_field, label_field, args):
+    '''
+        Load TabularDataset from path,
+        then convert it into a iterator
+        return TabularDataset and iterator
+    '''
+    tmp_data = data.TabularDataset(
+                            path=path,
+                            format='tsv',
+                            skip_header=False,
+                            fields=[
+                                    ('id', None),
+                                    ('question1', text_field),
+                                    ('question2', text_field),
+                                    ('label', label_field)
+                                    ])
+
+    tmp_iter = data.BucketIterator(
+                    dataset=tmp_data,
+                    batch_size=args.batch_size,
+                    device=1, # 0 for GPU, -1 for CPU
+                    sort_key=lambda x: len(x.question1) + len(x.question2),
+                    shuffle=False,
+                    repeat=False)
+    return tmp_data, tmp_iter
+
+
+def gen_iter_test(path, text_field, label_field, args):
+    '''
+        Load TabularDataset from path,
+        then convert it into a iterator
+        return TabularDataset and iterator
+    '''
+    tmp_data = data.TabularDataset(
+                            path=path,
+                            format='tsv',
+                            skip_header=False,
+                            fields=[
+                                    ('id', label_field),
+                                    ('question1', text_field),
+                                    ('question2', text_field)
+                                    ])
+
+    tmp_iter = data.Iterator(
+                    dataset=tmp_data,
+                    batch_size=args.batch_size,
+                    device=1, # 0 for GPU, -1 for CPU
+                    shuffle=False,
+                    repeat=False)
+    return tmp_data, tmp_iter
+
+
+def load_data(args):
+    # train woed2vec model
+    # *****************
+    # df_train = pd.read_csv('data/train_3000.tsv', names=['id', 'ques1', 'ques2', 'label'], sep='\t')
+    # word2vec_model = train_word2vec_model(df_train)
+    # word2vec_model.save(w2v_model_path)
+    # *****************
+    
+    text_field    = data.Field(sequential=True, use_vocab=True, 
+                    batch_first=True, eos_token='<EOS>', init_token='<BOS>', pad_token='<PAD>', tokenize=jieba.lcut)
+    label_field   = data.Field(sequential=False, use_vocab=False, batch_first=True)
+    
+    train_data, train_iter = gen_iter('data/train_3000.tsv', text_field, label_field, args)
+    dev_data, dev_iter     = gen_iter('data/valid_3000.tsv', text_field, label_field, args)
+    test_data, test_iter   = gen_iter_test(args.test_path, text_field, label_field, args)
+    text_field.build_vocab(train_data, dev_data)
+
+    return text_field, label_field, \
+        train_data, train_iter,\
+        dev_data, dev_iter,\
+        test_data, test_iter
+          
---
+++ code/submit/abcnn/train.py
@@ -0,0 +1,136 @@
+# coding = utf-8
+import os
+import sys
+import torch
+import torch.autograd as autograd
+import torch.nn.functional as F
+import torch.nn as nn
+import pandas as pd
+import traceback
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+
+def train(train_iter, dev_iter, model, args):
+    if args.cuda:
+        model.cuda()
+    parameters = list(filter(lambda p: p.requires_grad, model.parameters()))
+    optimizer = torch.optim.Adam(parameters, lr=args.lr)
+    steps = 0
+    best_f1 = 0
+    last_step = 0
+    
+    
+    for epoch in range(1, args.epochs+1): 
+        print('\nEpoch:%s\n'%epoch)
+
+        model.train()
+        for batch in train_iter:
+            res_list = []
+            label_list = []
+            question1, question2, target = batch.question1, batch.question2, batch.label
+            if args.cuda:
+                question1, question2, target = question1.cuda(), question2.cuda(), target.cuda()
+            optimizer.zero_grad()
+            
+            logit = model(question1, question2)
+            
+            
+            # ****** cosine_similarity *********
+            # target = target.type(torch.cuda.FloatTensor)
+            # criterion = nn.MSELoss()
+            # loss = criterion(logit, target)
+            
+            # ******* dot_product ************
+            target = target.type(torch.cuda.LongTensor)
+            weights = torch.cuda.FloatTensor([0.2, 0.8])
+            loss = F.cross_entropy(logit, target, weight=weights)
+            
+            loss.backward()
+            optimizer.step()
+            
+            
+            
+            steps += 1
+            if steps % args.log_interval == 0:
+                
+                # ******* dot_product ************
+                logit = logit.max(1)[1].cpu().numpy()
+                
+                res_list.extend(logit)
+                # ******* cosine_similarity ************
+                # threshold = 0.5    
+                # res_list = [1 if i > threshold else 0 for i in res_list]
+                label_list.extend(target.data.cpu().numpy())
+                acc = accuracy_score(res_list, label_list)
+                f1 = f1_score(res_list, label_list)
+                sys.stdout.write(
+                    '\rBatch[{}] - acc: {:.4f}, - f1: {:.4f}'.format(steps, acc, f1))
+            if steps % args.test_interval == 0:
+                dev_f1 = eval(dev_iter, model, args)
+                if dev_f1 > best_f1:
+                    best_f1 = dev_f1
+                    last_step = steps
+                    if args.save_best:
+                        save(model, args.save_dir, 'best', steps, best_f1)
+                else:
+                    if steps - last_step >= args.early_stop:
+                        print('early stop by {} steps.'.format(args.early_stop))
+
+
+
+def eval(data_iter, model, args):
+    model.eval()
+    res_list = []
+    label_list = []
+    for batch in data_iter:
+        question1, question2, target = batch.question1, batch.question2, batch.label
+        if args.cuda:
+            question1, question2, target = question1.cuda(), question2.cuda(), target.cuda()
+        logit = model(question1, question2)
+        # ******* cosine_sim ************
+        # target = target.type(torch.cuda.FloatTensor)
+        
+        # ******* dot_product ************
+        logit = logit.max(1)[1].cpu().numpy()
+        res_list.extend(logit)
+        label_list.extend(target.data.cpu().numpy()) 
+    # ******* cosine_sim ************
+    # threshold = 0.5
+    # res_list = [1 if i > threshold else 0 for i in res_list] 
+    f1 = f1_score(res_list, label_list)        
+    print('\nEvaluation -  f1: {:.4f} \n'.format(f1))
+    return f1
+
+
+def test(test_iter, model, args):
+    threshold = 0.5
+    res = []
+    for batch in test_iter:
+        qid, question1, question2 = batch.id, batch.question1, batch.question2
+        # if args.cuda:
+        #     qid, question1, question2 = qid.cuda(), question1.cuda(), question2.cuda()
+        results = model(question1, question2)
+        # print results
+        results = results.max(1)[1].cpu().numpy()
+        # print results
+        for i in range(len(qid.data)):
+            res.append([qid[i].data.cpu().numpy(), results[i]])
+            # if results[i].data >= threshold:
+            #     res.append([qid[i].data.cpu().numpy(), '1'])
+            # #elif results.data[i] < threshold:
+            # else:
+            #     res.append([qid[i].data.cpu().numpy(), '0'])
+    
+    # res = sorted(res, key=lambda x: x[0])
+    with open(args.res_path, 'w') as f:
+        cnt = 1
+        for x in res:
+            f.write('{}\t{}\n'.format(x[0], x[1]))
+            cnt += 1
+
+def save(model, save_dir, save_prefix, steps, f1):
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    save_prefix = os.path.join(save_dir, save_prefix)
+    save_path = '{}_steps_{}_{}.pt'.format(save_prefix, steps, f1)
+    torch.save(model.state_dict(), save_path)
+
