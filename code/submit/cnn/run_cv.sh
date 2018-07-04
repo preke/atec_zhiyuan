51a5bfb351542851e30487c2f51cd190f345ceed
preke <geek00021@gmail.com>
2018-07-04 16:20:06 +0800

    .

---
+++ code/submit/cnn/run_cv.sh
@@ -0,0 +1,26 @@
+python main.py_bp.py -train-path data/fold_13/train_fold_0.tsv -dev-path data/fold_13/valid_fold_0.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_1.tsv -dev-path data/fold_13/valid_fold_1.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_2.tsv -dev-path data/fold_13/valid_fold_2.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_3.tsv -dev-path data/fold_13/valid_fold_3.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_4.tsv -dev-path data/fold_13/valid_fold_4.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_5.tsv -dev-path data/fold_13/valid_fold_5.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_6.tsv -dev-path data/fold_13/valid_fold_6.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_7.tsv -dev-path data/fold_13/valid_fold_7.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_8.tsv -dev-path data/fold_13/valid_fold_8.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_9.tsv -dev-path data/fold_13/valid_fold_9.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_10.tsv -dev-path data/fold_13/valid_fold_10.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_11.tsv -dev-path data/fold_13/valid_fold_11.tsv
+python move_model.py
+python main.py_bp.py -train-path data/fold_13/train_fold_12.tsv -dev-path data/fold_13/valid_fold_12.tsv
+python move_model.py
