ed58a364b30eb87f61f2c707b4562455c483da3c
preke <geek00021@gmail.com>
2018-07-04 17:15:44 +0800

    .

--- code/submit/cnn/move_model.py
+++ code/submit/cnn/move_model.py
@@ -2,5 +2,5 @@ import os
 
 files = os.listdir('./snapshot/')
 files = sorted(files, key=lambda x:float(x.split('_')[3][:-3]), reverse=True)
-os.system('cp ' + files[0] + 'cv_models/')
+os.system('cp ' + files[0] + ' cv_models/')
 os.system('rm snapshot/*.pt')
