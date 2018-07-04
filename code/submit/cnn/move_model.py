125bdbd77ed7abfb4f7c1b2eb1100d8141ab51fa
preke <geek00021@gmail.com>
2018-07-04 16:10:22 +0800

    .

--- code/submit/cnn/move_model.py
+++ code/submit/cnn/move_model.py
@@ -2,5 +2,5 @@ import os
 
 files = os.listdir('./snapshot/')
 print files
-files = sorted(files, key=lambda x:float(x.split('_')[3]), reverse=True)
+files = sorted(files, key=lambda x:float(x.split('_')[3][:-3]), reverse=True)
 print files[0]
