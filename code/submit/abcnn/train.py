f773b4a687eb033b356d07c588be6fabcd3adf8e
preke <geek00021@gmail.com>
2018-07-06 14:25:35 +0800

    .

--- code/submit/abcnn/train.py
+++ code/submit/abcnn/train.py
@@ -61,7 +61,7 @@ def train(train_iter, dev_iter, model, args):
                 
                 res_list.extend(logit.data.cpu().numpy())
                 # ******* cosine_similarity ************
-                threshold = 0.3    
+                threshold = 0.5   
                 res_list = [1 if i > threshold else 0 for i in res_list]
                 label_list.extend(target.data.cpu().numpy())
                 acc = accuracy_score(res_list, label_list)
