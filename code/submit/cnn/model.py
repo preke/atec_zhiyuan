87735491f0a1672f141367266926dac33b60fa26
preke <geek00021@gmail.com>
2018-07-08 11:06:43 +0800

    .

--- code/submit/cnn/model.py
+++ code/submit/cnn/model.py
@@ -84,11 +84,11 @@ class CNN_Sim(nn.Module):
         cosine_value_cnn = F.cosine_similarity(q1_cnn, q2_cnn).view(-1, 1)        
         dot_value_cnn     = torch.bmm(q1_cnn.view(q1_cnn.size()[0], 1, q1_cnn.size()[1]), q2_cnn.view(q1_cnn.size()[0], q1_cnn.size()[1], 1)).view(q1_cnn.size()[0], 1)
         dist_value_cnn    = self.dist(q1_cnn, q2_cnn).view(q1_cnn.size()[0], 1)
-        mul_max_cnn = torch.max(q1_cnn * q2_cnn)
-        mul_min_cnn = torch.min(q1_cnn * q2_cnn)
+        mul_max_cnn = torch.max(q1_cnn * q2_cnn).view(-1, 1)
+        mul_min_cnn = torch.min(q1_cnn * q2_cnn).view(-1, 1)
 
-        sub_max_cnn = torch.abs(torch.max(torch.sub(q1_cnn, q2_cnn)))
-        sub_min_cnn = torch.abs(torch.min(torch.sub(q1_cnn, q2_cnn)))
+        sub_max_cnn = torch.abs(torch.max(torch.sub(q1_cnn, q2_cnn))).view(-1, 1)
+        sub_min_cnn = torch.abs(torch.min(torch.sub(q1_cnn, q2_cnn))).view(-1, 1)
         # mul_max_cnn = torch.max(q1_cnn * q2_cnn)
         
         # gru
@@ -100,10 +100,10 @@ class CNN_Sim(nn.Module):
         cosine_value_lstm1 = F.cosine_similarity(q1_seq_embedding, q2_seq_embedding).view(-1, 1)        
         dot_value_lstm1     = torch.bmm(q1_seq_embedding.view(q1_seq_embedding.size()[0], 1, q1_seq_embedding.size()[1]), q2_seq_embedding.view(q1_seq_embedding.size()[0], q1_seq_embedding.size()[1], 1)).view(q1_seq_embedding.size()[0], 1)
         dist_value_lstm1    = self.dist(q1_seq_embedding, q2_seq_embedding).view(q1_seq_embedding.size()[0], 1)
-        mul_max_lstm1 = torch.max(q1_seq_embedding * q1_seq_embedding)
-        mul_min_lstm1 = torch.min(q1_seq_embedding * q1_seq_embedding)
-        sub_max_lstm1 = torch.abs(torch.max(torch.sub(q1_seq_embedding, q1_seq_embedding)))
-        sub_min_lstm1 = torch.abs(torch.min(torch.sub(q1_seq_embedding, q1_seq_embedding)))
+        mul_max_lstm1 = torch.max(q1_seq_embedding * q1_seq_embedding).view(-1, 1)
+        mul_min_lstm1 = torch.min(q1_seq_embedding * q1_seq_embedding).view(-1, 1)
+        sub_max_lstm1 = torch.abs(torch.max(torch.sub(q1_seq_embedding, q1_seq_embedding))).view(-1, 1)
+        sub_min_lstm1 = torch.abs(torch.min(torch.sub(q1_seq_embedding, q1_seq_embedding))).view(-1, 1)
         
         ans = torch.cat((
             dot_value_lstm1, dist_value_lstm1, cosine_value_lstm1, dist_value_lstm1,
