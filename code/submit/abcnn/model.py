694fe9aa4f4ad9239a322f37427bd7210eb82108
preke <geek00021@gmail.com>
2018-07-20 15:07:05 +0800

    .

--- code/submit/abcnn/model.py
+++ code/submit/abcnn/model.py
@@ -59,6 +59,8 @@ class Abcnn1(nn.Module):
         x2 = x2.unsqueeze(1)
 
         sim = []
+        print self.ap[0](x1).shape
+
         sim.append(self.distance(self.ap[0](x1), self.ap[0](x2)))
 
         for i in range(self.layer_size):
